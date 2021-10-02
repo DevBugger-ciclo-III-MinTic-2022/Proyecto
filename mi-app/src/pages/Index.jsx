5d1f6658854caddaac4b60202b3ca0f5d6d50c89
Angie-Rivera <alejandrarivera1991@gmail.com>
2021-10-02 11:21:29 -0500

    pagina principal

--- mi-app/src/pages/Index.jsx
+++ mi-app/src/pages/Index.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 const Index = () => {
     return <nav>
             <ul className='bg-blue-50 w-2/5 mx-auto my-9 justify-between h-9 relative hover:'>
-                <li className='inline-block mx-6'>Ventas
+                <li className='inline-block mx-6 hover:text-green-300 mx-12 p-1'>Ventas
                     <ul className='absolute my-1 bg-blue-50'>
                         <li className='px-2 py-2 w-'><Link to='/ventas/registro' className='block no-underline hover:text-green-300'>Realizar Venta</Link></li>
                         <li className='px-2 py-2'><Link to='/ventas/productos' className='block no-underline hover:text-green-300'>Listado de Productos</Link></li>
