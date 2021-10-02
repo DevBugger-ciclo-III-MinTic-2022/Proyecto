16464995a89064e9292aa6ac6be5795cd83c8e99
Angie-Rivera <alejandrarivera1991@gmail.com>
2021-10-02 14:59:53 -0500

    Agregar bordes a la tabla

--- mi-app/src/pages/usuarios/Usuarios.jsx
+++ mi-app/src/pages/usuarios/Usuarios.jsx
@@ -2,48 +2,52 @@ import React from 'react';
 
 const Usuarios = () => {
     return <div>
-        <table className='w-2/5 bg-green-100 justify-between mx-auto my-24'>
+        <div className='flex w-2/5 justify-between'>
+            <h3 className='text-green-500'>Lista de Usuarios</h3>
+            <button className='border-2 rounded-full p-2'>Agregar usuario</button>
+        </div>
+        <table className='w-2/4 bg-green-100 justify-between mx-auto my-24 border-collapse border-2 border-green-600'>
             <tr>
-                <th>ID</th>
-                <th>Nombre</th>
-                <th>Correo</th>
-                <th>Rol</th>
-                <th>Acciones</th>
+                <th className='border-2 border-green-600'>ID</th>
+                <th className='border-2 border-green-600'>Nombre</th>
+                <th className='border-2 border-green-600'>Correo</th>
+                <th className='border-2 border-green-600'>Rol</th>
+                <th className='border-2 border-green-600'>Acciones</th>
             </tr>
             <tr>
-                <td>1</td>
-                <td>Angie Rivera</td>
-                <td>angierivera@gmail.com</td>
-                <td>Administrador</td>
-                <td>Editar/Eliminar</td>
+                <td className='border-2 border-green-600'>1</td>
+                <td className='border-2 border-green-600'>Angie Rivera</td>
+                <td className='border-2 border-green-600'>angierivera@gmail.com</td>
+                <td className='border-2 border-green-600'>Administrador</td>
+                <td className='border-2 border-green-600'>Editar/Eliminar</td>
             </tr>
             <tr>
-                <td>2</td>
-                <td>Santiago Villa</td>
-                <td>santivilla@gmail.com</td>
-                <td>Supervisor</td>
-                <td>Editar/Eliminar</td>
+                <td className='border-2 border-green-600'>2</td>
+                <td className='border-2 border-green-600'>Santiago Villa</td>
+                <td className='border-2 border-green-600'>santivilla@gmail.com</td>
+                <td className='border-2 border-green-600'>Supervisor</td>
+                <td className='border-2 border-green-600'>Editar/Eliminar</td>
             </tr>
             <tr>
-                <td>3</td>
-                <td>Pepito Perez</td>
-                <td>pepitoperez@gmail.com</td>
-                <td>Vendedor</td>
-                <td>Editar/Eliminar</td>
+                <td className='border-2 border-green-600'>3</td>
+                <td className='border-2 border-green-600'>Pepito Perez</td>
+                <td className='border-2 border-green-600'>pepitoperez@gmail.com</td>
+                <td className='border-2 border-green-600'>Vendedor</td>
+                <td className='border-2 border-green-600'>Editar/Eliminar</td>
             </tr>
             <tr>
-                <td>4</td>
-                <td>Jeronimo Correa</td>
-                <td>jerocorrea@gmail.com</td>
-                <td>Vendedor</td>
-                <td>Editar/Eliminar</td>
+                <td className='border-2 border-green-600'>4</td>
+                <td className='border-2 border-green-600'>Jeronimo Correa</td>
+                <td className='border-2 border-green-600'>jerocorrea@gmail.com</td>
+                <td className='border-2 border-green-600'>Vendedor</td>
+                <td className='border-2 border-green-600'>Editar/Eliminar</td>
             </tr>
             <tr>
-                <td>5</td>
-                <td>Juan Rodriguez</td>
-                <td>juanro20@gmail.com</td>
-                <td>Supervisor</td>
-                <td>Editar/Eliminar</td>
+                <td className='border-2 border-green-600'>5</td>
+                <td className='border-2 border-green-600'>Juan Rodriguez</td>
+                <td className='border-2 border-green-600'>juanro20@gmail.com</td>
+                <td className='border-2 border-green-600'>Supervisor</td>
+                <td className='border-2 border-green-600'>Editar/Eliminar</td>
             </tr>
         </table>
     </div>;
