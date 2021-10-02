3e94789ed69ca85f5e6f4d60f524c9e674f7aa01
Angie-Rivera <alejandrarivera1991@gmail.com>
2021-10-02 12:00:14 -0500

    Agregar enlaces a paginas

--- mi-app/src/pages/Index.jsx
+++ mi-app/src/pages/Index.jsx
@@ -3,18 +3,18 @@ import {Link} from 'react-router-dom';
 
 const Index = () => {
     return <nav>
-            <ul className='bg-blue-50 w-2/5 mx-auto my-9 justify-between h-9 relative hover:'>
-                <li className='inline-block mx-6 hover:text-green-300 mx-12 p-1'>Ventas
+            <ul className='bg-blue-50 w-2/5 mx-auto my-9 justify-between h-9 relative hover:text-green-300'>
+                <li className='inline-block mx-6'><span className='hover:text-green-300 text-blue-500 text-center p-1 mx-12'>Ventas</span>
                     <ul className='absolute my-1 bg-blue-50'>
                         <li className='px-2 py-2 w-'><Link to='/ventas/registro' className='block no-underline hover:text-green-300'>Realizar Venta</Link></li>
                         <li className='px-2 py-2'><Link to='/ventas/productos' className='block no-underline hover:text-green-300'>Listado de Productos</Link></li>
                         <li className='px-2 py-2'><Link to='/ventas/reportes' className='block no-underline hover:text-green-300'>Informe de Ventas</Link></li>
                     </ul> 
                 </li>   
-                <li className='inline-block mx-6'><a className='no-underline hover:text-green-300 mx-12 p-1' href="">Gestión de Usuarios</a>
+                <li className='inline-block mx-6'><span className='hover:text-green-300 text-blue-500 mx-12 p-1'>Gestión de Usuarios</span>
                     <ul className='absolute my-1 mx-3 bg-blue-50'>
-                        <li className='px-2 py-2'><a className='no-underline hover:text-green-300' href="">Listado de Usuarios</a></li>
-                        <li className='px-2 py-2'><a className='block no-underline hover:text-green-300' href="">Login</a></li>
+                        <li className='px-2 py-2'><Link to='/usuarios' className='no-underline hover:text-green-300'>Listado de Usuarios</Link></li>
+                        <li className='px-2 py-2'><Link to='/login' className='block no-underline hover:text-green-300'>Login</Link></li>
                     </ul>
                 </li>
             </ul>
