d18e32a9a6089b094e560b44216d50fd5ad77a0b
Santiago Villa Romero <90199102+svilla0130@users.noreply.github.com>
2021-10-05 16:00:37 -0500

    gestionar usuarios

--- src/pages/usuarios/Usuarios.jsx
+++ src/pages/usuarios/Usuarios.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 import home from 'media/home.png';
+
 
 const Usuarios = () => {
     return <div>
