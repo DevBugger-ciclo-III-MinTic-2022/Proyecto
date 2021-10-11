30bdca5f7db8e0bfc066a392b1e6fb108ef10563
Angie-Rivera <alejandrarivera1991@gmail.com>
2021-10-10 20:37:58 -0500

    Agregar cambios pagina de usuarios

--- mi-app/src/pages/admin/Usuarios.jsx
+++ mi-app/src/pages/admin/Usuarios.jsx
@@ -3,7 +3,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import axios from 'axios';
 import { nanoid } from 'nanoid';
 import { Dialog, Tooltip } from '@material-ui/core';
-import { obtenerProductos } from 'utils/api';
+import { obtenerUsuarios } from 'utils/api';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Usuarios = () => {
@@ -62,6 +62,302 @@ const Usuarios = () => {
         />
       )}
       <ToastContainer position='bottom-center' autoClose={5000} />
+    </div>
+  );
+};
+
+const TablaUsuarios = ({ listaUsuarios, setEjecutarConsulta }) => {
+  const [busqueda, setBusqueda] = useState('');
+  const [usuariosFiltrados, setUsuariosFiltrados] = useState(listaUsuarios);
+
+  useEffect(() => {
+    setUsuariosFiltrados(
+      listaUsuarios.filter((elemento) => {
+        return JSON.stringify(elemento).toLowerCase().includes(busqueda.toLowerCase());
+      })
+    );
+  }, [busqueda, listaUsuarios]);
+
+  return (
+    <div className='flex flex-col items-center justify-center w-full'>
+      <input
+        value={busqueda}
+        onChange={(e) => setBusqueda(e.target.value)}
+        placeholder='Buscar'
+        className='border-2 border-gray-700 px-3 py-1 self-start rounded-md focus:outline-none focus:border-indigo-500'
+      />
+      <h2 className='text-2xl font-extrabold text-gray-800'>Todos los usuarios</h2>
+      <div className='hidden md:flex w-full'>
+        <table className='tabla'>
+          <thead>
+            <tr>
+              <th>Nombre del usuario</th>
+              <th>Correo del usuario</th>
+              <th>Rol del usuario</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {usuariosFiltrados.map((usuario) => {
+              return (
+                <FilaUsuario
+                  key={nanoid()}
+                  usuario={usuario}
+                  setEjecutarConsulta={setEjecutarConsulta}
+                />
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      <div className='flex flex-col w-full m-2 md:hidden'>
+        {usuariosFiltrados.map((el) => {
+          return (
+            <div className='bg-gray-400 m-2 shadow-xl flex flex-col p-2 rounded-xl'>
+              <span>{el.nombre}</span>
+              <span>{el.correo}</span>
+              <span>{el.rol}</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+const FilaUsuario = ({ usuario, setEjecutarConsulta }) => {
+  const [edit, setEdit] = useState(false);
+  const [openDialog, setOpenDialog] = useState(false);
+  const [infoNuevoUsuario, setInfoNuevoUsuario] = useState({
+    nombre: usuario.nombre,
+    correo: usuario.correo,
+    rol: usuario.rol,
+  });
+
+  const actualizarUsuario = async () => {
+    //enviar la info al backend
+    const options = {
+      method: 'PATCH',
+      url: 'http://localhost:5000/usuarios/editar/',
+      headers: { 'Content-Type': 'application/json' },
+      data: { ...infoNuevoUsuario, id: usuario._id },
+    };
+
+    await axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        toast.success('Usuario modificado con éxito');
+        setEdit(false);
+        setEjecutarConsulta(true);
+      })
+      .catch(function (error) {
+        toast.error('Error modificando el usuario');
+        console.error(error);
+      });
+  };
+
+  const eliminarUsuario = async () => {
+    const options = {
+      method: 'DELETE',
+      url: 'http://localhost:5000/usuarios/eliminar/',
+      headers: { 'Content-Type': 'application/json' },
+      data: { id: usuario._id },
+    };
+
+    await axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        toast.success('Usuario eliminado con éxito');
+        setEjecutarConsulta(true);
+      })
+      .catch(function (error) {
+        console.error(error);
+        toast.error('Error eliminando el usuario');
+      });
+    setOpenDialog(false);
+  };
+
+  return (
+    <tr>
+      {edit ? (
+        <>
+          <td>
+            <input
+              className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2'
+              type='text'
+              value={infoNuevoUsuario.nombre}
+              onChange={(e) => setInfoNuevoUsuario({ ...infoNuevoUsuario, nombre: e.target.value })}
+            />
+          </td>
+          <td>
+            <input
+              className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2'
+              type='text'
+              value={infoNuevoUsuario.correo}
+              onChange={(e) =>
+                setInfoNuevoUsuario({ ...infoNuevoUsuario, correo: e.target.value })
+              }
+            />
+          </td>
+          <td>
+            <input
+              className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2'
+              type='text'
+              value={infoNuevoUsuario.rol}
+              onChange={(e) =>
+                setInfoNuevoUsuario({ ...infoNuevoUsuario, rol: e.target.value })
+              }
+            />
+          </td>
+        </>
+      ) : (
+        <>
+          <td>{usuario.nombre}</td>
+          <td>{usuario.correo}</td>
+          <td>{usuario.rol}</td>
+        </>
+      )}
+      <td>
+        <div className='flex w-full justify-around'>
+          {edit ? (
+            <>
+              <Tooltip title='Confirmar Edición' arrow>
+                <i
+                  onClick={() => actualizarUsuario()}
+                  className='fas fa-check text-green-700 hover:text-green-500'
+                />
+              </Tooltip>
+              <Tooltip title='Cancelar edición' arrow>
+                <i
+                  onClick={() => setEdit(!edit)}
+                  className='fas fa-ban text-yellow-700 hover:text-yellow-500'
+                />
+              </Tooltip>
+            </>
+          ) : (
+            <>
+              <Tooltip title='Editar Usuario' arrow>
+                <i
+                  onClick={() => setEdit(!edit)}
+                  className='fas fa-pencil-alt text-yellow-700 hover:text-yellow-500'
+                />
+              </Tooltip>
+              <Tooltip title='Eliminar Usuario' arrow>
+                <i
+                  onClick={() => setOpenDialog(true)}
+                  className='fas fa-trash text-red-700 hover:text-red-500'
+                />
+              </Tooltip>
+            </>
+          )}
+        </div>
+        <Dialog open={openDialog}>
+          <div className='p-8 flex flex-col'>
+            <h1 className='text-gray-900 text-2xl font-bold'>
+              ¿Está seguro de querer eliminar el usuario?
+            </h1>
+            <div className='flex w-full items-center justify-center my-4'>
+              <button
+                onClick={() => eliminarUsuario()}
+                className='mx-2 px-4 py-2 bg-green-500 text-white hover:bg-green-700 rounded-md shadow-md'
+              >
+                Sí
+              </button>
+              <button
+                onClick={() => setOpenDialog(false)}
+                className='mx-2 px-4 py-2 bg-red-500 text-white hover:bg-red-700 rounded-md shadow-md'
+              >
+                No
+              </button>
+            </div>
+          </div>
+        </Dialog>
+      </td>
+    </tr>
+  );
+};
+
+const FormularioCreacionUsuarios = ({ setMostrarTabla, listaUsuarios, setUsuarios }) => {
+  const form = useRef(null);
+
+  const submitForm = async (e) => {
+    e.preventDefault();
+    const fd = new FormData(form.current);
+
+    const nuevoUsuario = {};
+    fd.forEach((value, key) => {
+      nuevoUsuario[key] = value;
+    });
+
+    const options = {
+      method: 'POST',
+      url: 'http://localhost:5000/usuarios/nuevo/',
+      headers: { 'Content-Type': 'application/json' },
+      data: { nombre: nuevoUsuario.nombre, correo: nuevoUsuario.correo, rol: nuevoUsuario.rol },
+    };
+
+    await axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        toast.success('Usuario agregado con éxito');
+      })
+      .catch(function (error) {
+        console.error(error);
+        toast.error('Error creando un Usuario');
+      });
+
+    setMostrarTabla(true);
+  };
+
+  return (
+    <div className='flex flex-col items-center justify-center'>
+      <h2 className='text-2xl font-extrabold text-gray-800'>Crear nuevo usuario</h2>
+      <form ref={form} onSubmit={submitForm} className='flex flex-col'>
+        <label className='flex flex-col' htmlFor='nombre'>
+          Usuario
+          <input
+            name='nombre'
+            className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2'
+            type='text'
+            required
+          />
+        </label>
+        <label className='flex flex-col' htmlFor='correo'>
+          Correo del usuario
+          <input
+            className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2'
+            type='email'
+            name='correo'
+            required
+          />
+        </label>
+        <label className='flex flex-col' htmlFor='rol'>
+          Rol usuario
+          <select
+            className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2'
+            name='rol'
+            required
+            defaultValue={0}
+          >
+            <option disabled value={0}>
+              Seleccione una opción
+            </option>
+            <option>Administrador</option>
+            <option>Supervisor</option>
+            <option>Vendedor</option>
+          </select>
+        </label>
+
+        <button
+          type='submit'
+          className='col-span-2 bg-green-400 p-2 rounded-full shadow-md hover:bg-green-600 text-white'
+        >
+          Guardar usuario
+        </button>
+      </form>
     </div>
   );
 };
--- mi-app/src/utils/api.js
+++ mi-app/src/utils/api.js
@@ -12,3 +12,16 @@ export const obtenerProductos = async (setProductos, setEjecutarConsulta) => {
     });
   setEjecutarConsulta(false);
 };
+
+export const obtenerUsuarios = async (setUsuarios, setEjecutarConsulta) => {
+  const options = { method: 'GET', url: 'http://localhost:5000/usuarios' };
+  await axios
+    .request(options)
+    .then(function (response) {
+      setUsuarios(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+  setEjecutarConsulta(false);
+};
